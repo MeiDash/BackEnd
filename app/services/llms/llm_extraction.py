12b4d96ff037b886332ac6157a6307ae23936180
jjardelalves <jose.jardel.alves.medeiros@ccc.ufcg.edu.br>
2026-02-09 16:48:51 -0300

    ollama rodando no collab

--- app/services/llms/llm_extraction.py
+++ app/services/llms/llm_extraction.py
@@ -1,11 +1,16 @@
 import ollama
+from ollama import Client
+
+URL_REMOTA = "https://hiking-indiana-yard-luggage.trycloudflare.com"
+
+client = Client(host=URL_REMOTA)
 
 class LLMExtraction:
     def __init__(self):
         self.prompt = "Você é um extrator de informações. Extraia da nota o valor total, nome da empresa fornecedora, a data e o cnpj."
 
     def process(self, texto: str) -> str:
-        resp = ollama.chat(
+        resp = client.chat(
             model="llama3.2-vision:latest",
             messages=[
                 {"role": "system", "content": self.prompt},
